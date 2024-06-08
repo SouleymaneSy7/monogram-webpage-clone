2fb729638e05ec04302d167801eb18f8c961e449
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-08 07:30:30 +0200

    config: add an centralized import file for images and svgs

---
+++ src/utils/index.ts
@@ -0,0 +1,106 @@
+import packAudio from "@/assets/images/console-pack-audio-image.jpg";
+import packPhoto from "@/assets/images/console-pack-photo-image.jpg";
+import packVideo from "@/assets/images/console-pack-video-image.jpg";
+
+import dialModule from "@/assets/images/dial-module-image.jpg";
+import dialModuleHover from "@/assets/images/dial-module-image-hover.jpg";
+
+import essentialKey from "@/assets/images/essential-key-image.jpg";
+import essentialKeyHover from "@/assets/images/essential-key-image-hover.jpg";
+
+import hero1 from "@/assets/images/hero-image.jpg";
+import hero2 from "@/assets/images/hero-image-2.jpg";
+
+import miniConsole from "@/assets/images/mini-console-image.jpg";
+import miniConsoleHover from "@/assets/images/mini-console-image-hover.jpg";
+
+import monogramCare from "@/assets/images/monogram-care-image.jpg";
+import monogramCareHover from "@/assets/images/monogram-care-image-hover.jpg";
+
+import monogramCarying from "@/assets/images/monogram-carying-case-image.jpg";
+import monogramCaryingHover from "@/assets/images/monogram-carying-case-image-hover.jpg";
+
+import monogramCore from "@/assets/images/monogram-core-image.jpg";
+import monogramCoreHover from "@/assets/images/monogram-core-image-hover.jpg";
+
+import monogramKeyboard from "@/assets/images/monogram-keyboard-image.png";
+import monogramKeyboardHover from "@/assets/images/monogram-keyboard-image-hover.jpg";
+
+import monogramKeyboardMultipad from "@/assets/images/monogram-keyboard-multipad-image.png";
+import monogramKeyboardMultipadHover from "@/assets/images/monogram-keyboard-multipad-image-hover.jpg";
+
+import monogramMultipad from "@/assets/images/monogram-multipad-image.png";
+import monogramMultipadHover from "@/assets/images/monogram-multipad-image-hover.jpg";
+
+import orbiterModule from "@/assets/images/orbiter-module-image.jpg";
+import orbiterModuleHover from "@/assets/images/orbiter-module-image-hover.jpg";
+
+import sliderModule from "@/assets/images/slider-module-image.jpg";
+import sliderModuleHover from "@/assets/images/slider-module-image-hover.jpg";
+
+import audioConsole from "@/assets/images/audio-console-image.jpg";
+import audioConsoleHover from "@/assets/images/audio-console-image-hover.jpg";
+
+import photoConsole from "@/assets/images/photo-console-image.jpg";
+import photoConsoleHover from "@/assets/images/photo-console-image-hover.jpg";
+
+import videoConsole from "@/assets/images/video-console-image.jpg";
+import videoConsoleHover from "@/assets/images/video-console-image-hover.jpg";
+
+import logo from "@/assets/images/logo.svg";
+import cross from "@/assets/images/cross-icon.svg";
+import shop from "@/assets/images/shop-icon.svg";
+import chevronDown from "@/assets/images/chevron-down.svg";
+import hamburgerMenu from "@/assets/images/hamburger-menu.svg";
+import facebook from "@/assets/images/facebook-icon.svg";
+import instagram from "@/assets/images/instagram-icon.svg";
+import twitter from "@/assets/images/twitter-icon.svg";
+import youtube from "@/assets/images/youtube-icon.svg";
+
+export {
+  packAudio as packAudioImg,
+  packPhoto as packPhotoImg,
+  packVideo as packVideoImg,
+  dialModule as dialModuleImg,
+  dialModuleHover as dialModuleHoverImg,
+  essentialKey as essentialKeyImg,
+  essentialKeyHover as essentialKeyHoverImg,
+  hero1 as hero1Img,
+  hero2 as hero2Img,
+  miniConsole as miniConsoleImg,
+  miniConsoleHover as miniConsoleHoverImg,
+  monogramCare as monogramCareImg,
+  monogramCareHover as monogramCareHoverImg,
+  monogramCarying as monogramCaryingImg,
+  monogramCaryingHover as monogramCaryingHoverImg,
+  monogramCore as monogramCoreImg,
+  monogramCoreHover as monogramCoreHoverImg,
+  monogramKeyboard as monogramKeyboardImg,
+  monogramKeyboardHover as monogramKeyboardHoverImg,
+  monogramKeyboardMultipad as monogramKeyboardMultipadImg,
+  monogramKeyboardMultipadHover as monogramKeyboardMultipadHoverImg,
+  monogramMultipad as monogramMultipadImg,
+  monogramMultipadHover as monogramMultipadHoverImg,
+  orbiterModule as orbiterModuleImg,
+  orbiterModuleHover as orbiterModuleHoverImg,
+  sliderModule as sliderModuleImg,
+  sliderModuleHover as sliderModuleHoverImg,
+  photoConsole as photoConsoleImg,
+  photoConsoleHover as photoConsoleHoverImg,
+  audioConsole as audioConsoleImg,
+  audioConsoleHover as audioConsoleHoverImg,
+  videoConsole as videoConsoleImg,
+  videoConsoleHover as videoConsoleHoverImg,
+};
+
+export {
+  logo as logoIcon,
+  cross as crossIcon,
+  shop as shopIcon,
+  chevronDown as chevronDownIcon,
+  hamburgerMenu as hamburgerMenuIcon,
+  facebook as facebookIcon,
+  instagram as instagramIcon,
+  twitter as twitterIcon,
+  youtube as youtubeIcon,
+};
