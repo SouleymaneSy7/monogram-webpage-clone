2a8446905b14c82a4f829f2f3fb6b106f4d98c06
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-12 01:54:59 +0200

    feat: add reusable cards component

---
+++ src/components/Cards.tsx
@@ -0,0 +1,77 @@
+import Button from "./Button";
+
+type CardPropsTypes = {
+  cardImg: string;
+  cardImgHover?: string | undefined;
+  cardTitle: string;
+  cardDescription?: string | null;
+  cardPrice: string;
+};
+
+const Cards = ({
+  cardImg,
+  cardImgHover,
+  cardTitle,
+  cardDescription,
+  cardPrice,
+}: CardPropsTypes) => {
+  let description;
+  let imgHover;
+  let badge;
+
+  if (cardDescription) {
+    description = (
+      <p className="card__footer__description">{cardDescription}</p>
+    );
+  }
+
+  if (cardImgHover) {
+    imgHover = (
+      <img
+        src={cardImgHover}
+        className="card__img card__img--hover"
+        alt="Product Images"
+      />
+    );
+
+    badge = (
+      <div className="card__badge">
+        <p>Pre-order</p>
+      </div>
+    );
+  }
+
+  return (
+    <article className="card">
+      <header className="card__header">
+        <a href="#" title={cardTitle}>
+          <div className="card__header__img">
+            <img src={cardImg} className="card__img" alt="Products Images" />
+            {imgHover}
+          </div>
+
+          {badge}
+        </a>
+      </header>
+
+      <footer className="card__footer">
+        <a href="#" title={cardTitle}>
+          <div className="card__footer__flex">
+            <h2 className="card__footer__title">{cardTitle}</h2>
+
+            <div>
+              <p className="card__footer__price">{cardPrice}</p>
+              <Button type="button" classNames="card__footer--btn">
+                Shop Now
+              </Button>
+            </div>
+          </div>
+
+          {description}
+        </a>
+      </footer>
+    </article>
+  );
+};
+
+export default Cards;
