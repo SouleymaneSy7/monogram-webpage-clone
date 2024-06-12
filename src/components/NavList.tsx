32b91d7af699869130a664495b7b0115e45d67e7
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-12 08:13:11 +0200

    fix: fix issues for reusability of the components

--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -1,15 +1,37 @@
 import DropDown from "./DropDown";
+import Icons from "./Icons";
 type NavListPropsTypes = {
   list: {
     title: string;
-    dropDownMenu?: { id: number; dropDownTitle: string }[];
+    icon?: string | undefined;
+    dropDownMenu?: {
+      id: number;
+      dropDownTitle: string;
+    }[];
   };
 };
 const NavList = ({ list }: NavListPropsTypes) => {
+  const { title, icon } = list;
+
+  let label;
+
+  if (icon) {
+    label = (
+      <div>
+        <Icons iconSrc={icon} alt="Shop icons" />
+        {title}
+      </div>
+    );
+  }
+
   return (
     <li>
       {list.dropDownMenu ? (
-        <DropDown label={list.title} options={list.dropDownMenu} />
+        list.icon ? (
+          <DropDown label={label} options={list.dropDownMenu} />
+        ) : (
+          <DropDown label={list.title} options={list.dropDownMenu} />
+        )
       ) : (
         <a href="#">{list.title}</a>
       )}
