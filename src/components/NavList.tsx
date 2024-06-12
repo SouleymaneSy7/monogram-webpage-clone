f19c801057ad7b0a08a46d0bf08343c284b2523b
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-13 01:40:41 +0200

    fix: fix the icons not showing issues in the NavList components

--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -1,5 +1,6 @@
 import DropDown from "./DropDown";
 import Icons from "./Icons";
+
 type NavListPropsTypes = {
   list: {
     title: string;
@@ -10,30 +11,16 @@ type NavListPropsTypes = {
     }[];
   };
 };
+
 const NavList = ({ list }: NavListPropsTypes) => {
-  const { title, icon } = list;
-
-  let label;
-
-  if (icon) {
-    label = (
-      <div>
-        <Icons iconSrc={icon} alt="Shop icons" />
-        {title}
-      </div>
-    );
-  }
-
   return (
     <li>
       {list.dropDownMenu ? (
-        list.icon ? (
-          <DropDown label={label} options={list.dropDownMenu} />
-        ) : (
-          <DropDown label={list.title} options={list.dropDownMenu} />
-        )
+        <DropDown label={list.title} options={list.dropDownMenu} />
       ) : (
-        <a href="#">{list.title}</a>
+        <a href="#">
+          {list.title || <Icons iconSrc={list.icon} alt="Shop Bag icon" />}
+        </a>
       )}
     </li>
   );
