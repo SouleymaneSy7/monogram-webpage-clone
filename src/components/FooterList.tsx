bc0d7287b83ba81393f3eb314dbf48c0e794a420
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-23 06:09:33 +0200

    fix: fix the issues

--- src/components/FooterList.tsx
+++ src/components/FooterList.tsx
@@ -4,20 +4,18 @@ import { footerLinks, footerCopyright } from "@/constants";
 const FooterList = () => {
   return (
     <React.Fragment>
-      <nav className="footer__children footer--nav">
-        {footerLinks.map(({ id, title, linkList }) => (
-          <div key={id}>
-            <h3 className="footer--nav__title">{title}</h3>
-            <ul className="footer--nav__list">
-              {linkList.map((link) => (
-                <li key={link} className="footer--nav__list__item">
-                  <a href="#">{link}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </nav>
+      {footerLinks.map(({ id, title, linkList }) => (
+        <div key={id} className="footer__children footer--nav">
+          <h3 className="footer--nav__title">{title}</h3>
+          <ul className="footer--nav__list">
+            {linkList.map((link) => (
+              <li key={link} className="footer--nav__list__item">
+                <a href="#">{link}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
 
       <div className="footer__children footer--copyright">
         <p className="footer--copyright__text">{footerCopyright}</p>
