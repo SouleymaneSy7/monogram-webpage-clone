37b948e3b649782e656c12d3462181aeb1de2f32
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-24 03:00:29 +0200

    feat: add Header, Main and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <React.Fragment>
       <Header />
+      <Main />
+      <Footer />
     </React.Fragment>
   );
 };
