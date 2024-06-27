602378f9c7b367dac565533676ce391e5731fe7c
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-28 00:43:30 +0200

    feat: add responsive mobile navbar

--- src/components/HeaderMobileNavbar.tsx
+++ src/components/HeaderMobileNavbar.tsx
@@ -1,29 +1,56 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
 import { CrossIcon, LogoIcon } from "@/utils";
 import { headerMobileNavlist } from "@/constants";
 import Button from "./Button";
 import NavList from "./NavList";
 
-const HeaderMobileNavbar = () => {
-  return (
-    <div className="mobile-navbar">
-      <div className="mobile-navbar__header">
-        <a href="#" className="mobile-navbar__logo">
-          <LogoIcon height={38} fill="var(--link-clr)" />
-        </a>
+const mobileNavDOM = document.querySelector("#mobile-navbar-root");
 
-        <Button type="button" className="mobile-navbar--btn">
-          <CrossIcon width={36} height={36} fill="var(--link-clr)" />
-        </Button>
+type HeaderMobileNavbarPropsTypes = {
+  isOpen: boolean;
+  handleOpen: () => void;
+};
+
+const HeaderMobileNavbar = ({
+  isOpen,
+  handleOpen,
+}: HeaderMobileNavbarPropsTypes) => {
+  return createPortal(
+    <React.Fragment>
+      <div
+        className={
+          isOpen ? "mobile-navbar-container active" : "mobile-navbar-container"
+        }
+      >
+        <div className="mobile-navbar">
+          <div className="mobile-navbar__header">
+            <a href="#" className="mobile-navbar__logo">
+              <LogoIcon height={38} fill="var(--link-clr)" />
+            </a>
+
+            <Button type="button" className="mobile-navbar--btn">
+              <CrossIcon
+                width={36}
+                height={36}
+                fill="var(--link-clr)"
+                onClick={handleOpen}
+              />
+            </Button>
+          </div>
+
+          <nav className="mobile-navbar__navigation">
+            <ul className="mobile-navbar__navigation__list">
+              {headerMobileNavlist.map((navList) => {
+                return <NavList list={navList} key={navList.id} />;
+              })}
+            </ul>
+          </nav>
+        </div>
       </div>
-
-      <nav className="mobile-navbar__navigation">
-        <ul className="mobile-navbar__navigation__list">
-          {headerMobileNavlist.map((navList) => {
-            return <NavList list={navList} key={navList.id} />;
-          })}
-        </ul>
-      </nav>
-    </div>
+    </React.Fragment>,
+    mobileNavDOM!
   );
 };
 
--- src/components/HeaderNavList.tsx
+++ src/components/HeaderNavList.tsx
@@ -1,46 +1,66 @@
 import React from "react";
+
 import { MobileDropDown, headerDesktopNavlist } from "@/constants";
 import Button from "./Button";
 import DropDown from "./DropDown";
 import NavList from "./NavList";
 import { LogoIcon, HamburgerMenuIcon, ShopIcon } from "@/utils";
+import HeaderMobileNavbar from "./HeaderMobileNavbar";
 
 const HeaderNavList = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   const { dropDownTitle } = MobileDropDown[0];
 
   const newTitle = dropDownTitle.replace(" ($)", "");
 
   return (
     <React.Fragment>
-      <nav className="navigation">
-        <a href="#" className="navigation__logo">
-          <LogoIcon fill="currentColor" />
-        </a>
+      <nav className="navigation ">
+        <div className="container">
+          <a href="#" className="navigation__logo">
+            <LogoIcon fill="currentColor" />
+          </a>
 
-        <div className="navigation--mobile">
-          <Button type="button" className="navigation--mobile--hamburger-menu">
-            <HamburgerMenuIcon width="30" height="30" fill="var(--link-clr)" />
-          </Button>
-
-          <div className="navigation--mobile--shop-btn">
-            <Button type="button">
-              <ShopIcon width="25" height="25" fill="var(--link-clr)" />
+          <div className="navigation--mobile">
+            <Button
+              type="button"
+              className="navigation--mobile--hamburger-menu"
+              onClick={handleOpen}
+            >
+              <HamburgerMenuIcon
+                width="30"
+                height="30"
+                fill="var(--link-clr)"
+              />
             </Button>
+
+            <div className="navigation--mobile--shop-btn">
+              <Button type="button">
+                <ShopIcon width="25" height="25" fill="var(--link-clr)" />
+              </Button>
+            </div>
+
+            <div className="navigation--mobile--dropdown">
+              <DropDown label={newTitle} options={MobileDropDown} />
+            </div>
           </div>
 
-          <div className="navigation--mobile--dropdown">
-            <DropDown label={newTitle} options={MobileDropDown} />
+          <div className="navigation--desktop">
+            <ul className="navigation--desktop__list">
+              {headerDesktopNavlist.map((navList, index) => {
+                return <NavList list={navList} key={index} />;
+              })}
+            </ul>
           </div>
-        </div>
-
-        <div className="navigation--desktop">
-          <ul className="navigation--desktop__list">
-            {headerDesktopNavlist.map((navList, index) => {
-              return <NavList list={navList} key={index} />;
-            })}
-          </ul>
         </div>
       </nav>
+
+      <HeaderMobileNavbar isOpen={isOpen} handleOpen={handleOpen} />
     </React.Fragment>
   );
 };
